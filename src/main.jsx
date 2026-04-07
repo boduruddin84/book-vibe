491d24f6dfdd795cfc988550f59e5482afb9d214
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-04-07 16:35:46 +0600

    NavBar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { router } from './routes/Routes'
+import { RouterProvider } from 'react-router'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: "Homepage",
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
